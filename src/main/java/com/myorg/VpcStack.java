bb175e6d8f48c450aae93c2a4cea5732c464d80a
Williamaia <williamaiadias@hotmail.com>
2023-01-05 16:38:00 -0300

    ...

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -1,2 +1,22 @@
-package com.myorg;public class VpcStack {
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+public class VpcStack extends Stack {
+    private Vpc vpc;
+    public VpcStack(final Construct scope, final String id){this(scope, id, null);}
+    public VpcStack(final Construct scope, String id, final StackProps props){
+        super(scope, id, props);
+
+        Vpc.Builder.create(this, "Vpc01")
+                .maxAzs(3)
+                .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
 }
